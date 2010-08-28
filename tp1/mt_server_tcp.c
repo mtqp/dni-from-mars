b9936eb9894d888763468c36067292397d4fca1b
mariansabianaa <mariansabianaa@61979caa-1c78-702c-cbbc-9e42fa75e581>
2010-08-28 17:43:50 +0000

    Empezando la remake

--- tp1/mt_server_tcp.c
+++ tp1/mt_server_tcp.c
@@ -9,7 +9,7 @@ int main()
 	char output[MAX_MSG_LENGTH];
 	int output_size;
 	/* Crear socket sobre el que se lee: dominio INET, protocolo TCP (STREAM)*/
-	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock < 0) {
 		perror("abriendo socket");
 		exit(1);
@@ -30,8 +30,8 @@ int main()
 		printf("Comando: %s", buf);
 		fd = popen(buf,"r");
 		output_size = fread( output, sizeof(char), MAX_MSG_LENGTH, fd );
-		/*output[output_size]=0;
-		sendto( sock, output, output_size, 0, (struct sockaddr*) &name, sizeof(name));*/
+		output[output_size]=0;
+		sendto( sock, output, output_size, 0, (struct sockaddr*) &name, sizeof(name));
 		fclose(fd);
 	}
 	/* Cerrar socket de recepción. */
